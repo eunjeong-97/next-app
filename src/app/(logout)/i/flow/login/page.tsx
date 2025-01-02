363716606b04229aee1eaf475b19dff554a21843
eunjeong-97 <eoss7777@naver.com>
2025-01-02 14:00:21 +0900

    ch2-4. template.tsx, Link, Image, redirect
    1. template.tsx
    페이지가 변경되어도 페이지 요소만 리렌더링될 뿐, 레이아웃은 기본적으로 리렌더링되지 않는다
    그래서 레이아웃도 리렌더링하고 싶은 경우 template.tsx 를 사용하면 되는데 layout.tsx와 같이 사용하면 안된다.
    
    2. a태그 대신 Link태그
    a태그는 페이지가 새로고침되면서 이동하는데 React나 Next에선 페이지 새로고침이 안되야 하기 때문에 a태그 대신에 Link 컴포넌트를 사용한다
    
    3. 로그인버튼을 누르면 /login 경로로 이동한 다음 /i/flow/login 경로로 리다이렉팅되는데 page.tsx, route,tsx 두가지 방법 중 page.tsx 를 사용한다.
    
    4. Next는 public 폴더에서 png파일을 import할 수 있는데 단순 image 태그가 아닌, next/image 에서 가져온 Image 컴포넌트를 사용해야 한다. 이 Image 컴포넌트는 Next에서 이미지를 자동으로 최적화처리를 해준다.

--- src/app/(logout)/i/flow/login/page.tsx
+++ src/app/(logout)/i/flow/login/page.tsx
@@ -1,3 +1,12 @@
+import Image from "next/image";
+
+import zlogo from "@/../public/zlogo.png";
+
 export default function LoginPage() {
-  return <div>i/flow/login 페이지</div>;
+  return (
+    <div>
+      i/flow/login 페이지
+      <Image src={zlogo} alt="logo" />
+    </div>
+  );
 }
